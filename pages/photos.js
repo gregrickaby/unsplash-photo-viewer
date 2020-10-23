912e7ef721c1aa61331ef41ae44ad1e653620fbc
Greg Rickaby <greg@gregrickaby.com>
2020-10-22 20:03:48 -0500

    map over and link to single photo

--- pages/photos.js
+++ pages/photos.js
@@ -1,9 +1,29 @@
+import PropTypes from 'prop-types'
 import {getPhotos} from '@/api/getPhotos'
+import Link from 'next/link'
 
 export default function Photos(props) {
   return (
     <>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      {props.photos.map((photo) => {
+        const {
+          id,
+          alt_description,
+          urls: {regular}
+        } = photo
+        return (
+          <Link href={`/photo/${id}`} key={id}>
+            <a aria-label={alt_description}>
+              <img
+                src={regular}
+                alt={alt_description}
+                loading="lazy"
+                width="1080"
+              />
+            </a>
+          </Link>
+        )
+      })}
     </>
   )
 }
@@ -16,4 +36,8 @@ export async function getStaticProps() {
     },
     revalidate: 5
   }
+}
+
+Photos.propTypes = {
+  photos: PropTypes.array
 }
