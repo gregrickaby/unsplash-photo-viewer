b0220b42dba352c0890a5084e474dcac720fe63d
Greg Rickaby <greg@gregrickaby.com>
2020-11-08 15:34:43 -0600

    style header

--- components/organisms/Header.js
+++ components/organisms/Header.js
@@ -1,21 +1,18 @@
+import config from '@/lib/config'
+import cn from 'classnames'
 import Link from 'next/link'
-import Image from 'next/image'
+import styles from './Header.module.css'
 
 export default function Header() {
   return (
-    <header className="sticky top-0 py-8 transition-all bg-white border-b border-gray-200">
-      <div className="container px-4 lg:px-0 flex items-center justify-between">
+    <header className={styles.header}>
+      <div className={cn('wrap', styles.container)}>
         <Link href="/">
           <a>
-            <Image
-              src="/logo.svg"
-              alt="website logo"
-              loading="lazy"
-              height="128"
-              width="128"
-            />
+            <h1 className={styles.siteName}>{config.siteName}</h1>
           </a>
         </Link>
+        <p className={styles.siteDescription}>{config.siteDescription}</p>
       </div>
     </header>
   )
