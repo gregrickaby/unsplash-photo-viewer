135ec3f3aff3904adb9900161b97e3df5e27f236
Greg Rickaby <greg@gregrickaby.com>
2020-11-13 16:04:36 -0600

    use next/image

--- components/Card.js
+++ components/Card.js
@@ -1,15 +1,22 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 
-export default function Card(props) {
-  const {id, alt, src} = props
+export default function Card(data) {
+  const {id, alt, source, height, width} = data
 
   return (
     <article className="card">
       <Link href={`/photo/${id}`}>
         <a aria-label={alt}>
           <div className="card-container">
-            <img src={src} alt={alt} loading="lazy" />
+            <Image
+              src={source}
+              alt={alt}
+              layout="responsive"
+              width={width}
+              height={height}
+            />
           </div>
         </a>
       </Link>
@@ -18,7 +25,11 @@ export default function Card(props) {
 }
 
 Card.propTypes = {
-  id: PropTypes.string,
-  alt: PropTypes.string,
-  src: PropTypes.string
+  data: PropTypes.shape({
+    alt: PropTypes.string,
+    height: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired
+  })
 }
