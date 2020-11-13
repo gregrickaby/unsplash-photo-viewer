5e55bc672612ba073f309741b0b6e01ccbd704e9
Greg Rickaby <greg@gregrickaby.com>
2020-11-12 22:25:51 -0600

    refactor and style single

--- pages/photo/[id].js
+++ pages/photo/[id].js
@@ -1,7 +1,10 @@
 import {getPhotoById, getPhotos} from '@/api/getPhotos'
 import Layout from '@/components/common/Layout'
+import Image from 'next/image'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
+import {useRef, useState} from 'react'
+import styles from './single.module.css'
 
 export default function Photo({photo}) {
   const {
@@ -12,7 +15,7 @@ export default function Photo({photo}) {
     likes,
     views,
     downloads,
-    urls: {regular},
+    urls: {full},
     exif: {make, model, exposure_time, aperture, focal_length, iso},
     user: {
       name,
@@ -21,49 +24,84 @@ export default function Photo({photo}) {
     }
   } = photo
 
+  const [dialog, setDialog] = useState(false)
+
+  const detailsDialog = useRef(null)
+
+  function toggleDialog() {
+    if (dialog) {
+      setDialog(false)
+      detailsDialog.current.close()
+    }
+
+    if (!dialog) {
+      setDialog(true)
+      detailsDialog.current.show()
+    }
+  }
+
   return (
     <Layout>
-      <div className="wrap">
-        <h1>{description}</h1>
-        <img src={regular} alt={alt_description} loading="lazy" />
-        <div>
-          <p>
-            <strong>Photographer</strong>
-          </p>
-          <a href={html}>
-            <img src={small} height={32} width={32} alt={name} />
-            <p>{name}</p>
-          </a>
-        </div>
+      <article className={styles.single}>
+        <figure className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            src={full}
+            alt={alt_description}
+            layout="responsive"
+            width={width}
+            height={height}
+          />
+        </figure>
 
-        <div>
-          <p>
-            <strong>Social Stats</strong>
-          </p>
-          <p>Likes {likes.toLocaleString('en')}</p>
-          <p>Views {views.toLocaleString('en')}</p>
-          <p>Downloads {downloads.toLocaleString('en')}</p>
-        </div>
+        <dialog
+          className={styles.dialog}
+          ref={detailsDialog}
+          onKeyDown={(e) => {
+            if (e.code === 'Escape') toggleDialog()
+          }}
+        >
+          <header className={styles.header}>
+            <h1 className={styles.title}>{description}</h1>
+          </header>
 
-        <div>
-          <p>
-            <strong>Technical Details</strong>
-          </p>
-          <p>Camera Make {make}</p>
-          <p>Camera Model {model}</p>
-          <p>Focal Length {focal_length}</p>
-          <p>Aperture ƒ/{aperture}</p>
-          <p>Shutter Speed {exposure_time}s</p>
-          <p>ISO {iso}</p>
-          <p>
-            Dimensions {width} × {height}
-          </p>
-        </div>
+          <div className={styles.details}>
+            <div className={styles.credits}>
+              <strong>Photographer</strong>
+              <div className={styles.photographer}>
+                <img src={small} height={32} width={32} alt={name} />
+                <a href={html}>
+                  <p>{name}</p>
+                </a>
+              </div>
+            </div>
+            <div className={styles.social}>
+              <strong>Social Stats</strong>
+              <p>Likes: {likes.toLocaleString('en')}</p>
+              <p>Views: {views.toLocaleString('en')}</p>
+              <p>Downloads: {downloads.toLocaleString('en')}</p>
+            </div>
+            <div className={styles.technical}>
+              <strong>Technical Details</strong>
+              <p>Camera Make: {make}</p>
+              <p>Camera Model: {model}</p>
+              <p>Focal Length: {focal_length}</p>
+              <p>Aperture: ƒ/{aperture}</p>
+              <p>Shutter Speed: {exposure_time}s</p>
+              <p>ISO: {iso}</p>
+              <p>
+                Dimensions: {width} × {height}
+              </p>
+            </div>
+          </div>
+        </dialog>
+
+        <button onClick={toggleDialog}>Show info</button>
 
         <Link href="/">
           <a>Go Back</a>
         </Link>
-      </div>
+      </article>
     </Layout>
   )
 }
