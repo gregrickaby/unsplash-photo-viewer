8fa50a83a5fb99b66806bbd78fcd319e5549fd88
Greg Rickaby <gregrickaby@gmail.com>
2020-12-23 16:25:18 -0600

    link the title back home

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
 import config from '@/functions/config'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="site-title">{config.siteName}</h1>
+      <h1 className="site-title">
+        <Link href="/">
+          <a>{config.siteName}</a>
+        </Link>
+      </h1>
       <p className="site-description">{config.siteDescription}</p>
     </header>
   )
